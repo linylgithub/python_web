b1ffa3ca76a144b9e7d8644402e2e81bc2d2b2be
linyl <lylyoung@outlook.com>
2018-11-25 16:15:16 +0800

    新增flask_restful

---
+++ chapter4/section2/app_restful.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+"""
+# Author: linyl
+# Created Time : Sat Nov 24 23:40:49 2018
+# File Name: app_restful.py
+# Description:
+"""
+
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('config')
+api = Api(app)
+db = SQLAlchemy(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('admin', type=bool, help='Use super manager mode', default=False)
+
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'address': fields.String
+}
+
+class User(db.Model):
+    __tablename__ = 'restful_user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    address = db.Column(db.String(128), nullable=False)
+
+db.create_all()
+
+class UserResource(Resource):
+    """API"""
+    @marshal_with(resource_fields)
+    def get(self, name):
+        user = User.query.filter_by(name=name).first()
+        return user
+    
+    def put(self, name):
+        address = request.form.get('address', '')
+        user = User(name=name, address=address)
+        db.session.add(user)
+        db.session.commit()
+        return {'ok':0}, 201
+
+    def delete(self, name):
+        args = parser.parse_args()
+        is_admin = args['admin']
+        if not is_admin:
+            return {'error': 'You do not have permissions'}
+        user = User.query.filter_by(name=name).first()
+        db.session.delete(user)
+        db.session.commit()
+        return {'ok':0}
+api.add_resource(UserResource, '/users/<name>')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9000)
+
