89439fe70cff65ade02a95dc18b435773479635f
linyl <lylyoung@outlook.com>
2018-11-26 23:43:08 +0800

    ajax

---
+++ chapter5/app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+"""
+# Author: linyl
+# Created Time : Mon Nov 26 23:25:42 2018
+# File Name: app.py
+# Description:
+"""
+
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
+
+@app.route('/')
+def index():
+    return render_template('chapter5/signin.html')
+
+@app.route('/signin/', methods=['POST'])
+def signin():
+    username = request.form['username']
+    password = request.form['password']
+    error = None
+    if len(username) < 5:
+        error = 'Username must be at least 5 characters'
+
+    if len(password) < 6:
+        error = 'Password must be at least 6 characters'
+    elif not any(c.isupper() for c in password):
+        error = "Your password needs at least 1 capital"
+    if error is not None:
+        return jsonify({'r': 1, 'error': error})
+    return jsonify({'r': 0, 'rs': 'Ok'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9000)
